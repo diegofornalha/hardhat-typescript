47877ef15d9c132ca658f1a5994ead2cfe2bd684
Diego Fornalha <diegofornalha@gmail.com>
2022-05-15 23:50:48 -0300

    hardhat config na sua origem

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,4 @@
 require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-web3");
-
-require("@nomiclabs/hardhat-waffle");
-
-module.exports = {
-  solidity: "0.8.0",
-  networks: {
-    rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/rzm9Th3XsMB4NpQgIrd4-szgJp7HSHx9",
-      accounts: ["8e87f7e6c6717a22064bb73acb6651e68b946c24ebda2e5f8352e35148305249"],
-    },
-  },
-};
-
-task("balance", "Prints an account's balance")
-  .addParam("account", "The account's address")
-  .setAction(async (taskArgs) => {
-    const account = web3.utils.toChecksumAddress(taskArgs.account);
-    const balance = await web3.eth.getBalance(account);
-
-    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
-  });
-
-module.exports = {};
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -31,7 +7,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-
 
 module.exports = {
   solidity: "0.8.4",
