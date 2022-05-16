9609d15e84c5d740f692feb3635ae9814b402286
Diego Fornalha <diegofornalha@gmail.com>
2022-05-15 23:58:02 -0300

    hardhat config padrão

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: "https://eth-goerli.alchemyapi.io/v2/_bEj1iL2DfptrO0R2jsDrHAhZPKRP5eU",
+      accounts: ["ead8546c078edc2522d3130f231e74bf521e29cc2e76d005ce82f93c798873ef"],
+    },
+  },
 };
