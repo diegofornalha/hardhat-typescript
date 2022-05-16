dfed23deeceaeec7280cd3f9f30b0a47bd2bd6cb
Diego Fornalha <diegofornalha@gmail.com>
2022-05-15 22:40:05 -0300

    novo plugin adicionado para saber o saldo de uma conta

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
+
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+  });
+
+module.exports = {};
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
